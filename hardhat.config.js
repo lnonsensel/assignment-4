fa6efb14d37d7fa56865ad15588aa596619ba8e6
ltt <ltt@MacBook-Pro-ltt.local>
2023-04-04 10:32:06 +0300

    Added gas-reporter and coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
 
   networks:{
     bnbt:{
-      url:"https://indulgent-newest-leaf.bsc-testnet.discover.quiknode.pro/8a8483b20d028a2b4ca603f1f86a6f1d702a6624/",
+      url: process.env.BNBt_RPC_URL,
       accounts:[
-        "14514ffed7250147e426e8a2c55aeec04ad9cc9ad045d94f57eb27ec80a47b6d",
+        process.env.PRIVATE_KEY,
       ],
     },
   },
   etherscan:{
-    apiKey: "4415P85IB4M5H5DR6Q2FKYWU4XENPX1YG9",
+    apiKey: process.env.ETHERSCAN_API,
   },
   gasReporter: {
     enabled: true,
     noColors: true,
+    currency: "USD",
+    token: "BNB",
+    gasPriceApi: "https://api.bscscan.com/api?module=proxy&action=eth_gasPrice",
+    coinmarketcap: process.env.COINMARKETCAP_API,
+    outputFile: "gasReporter.txt",
   },
 };
